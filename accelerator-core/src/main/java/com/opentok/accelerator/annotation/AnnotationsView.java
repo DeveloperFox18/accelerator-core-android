5e983bec14fa28cd4c3fe474fb99f075495cbe33
DeveloperFox18 <developer.fox18@gmail.com>
2024-09-25 15:58:05 +0530

    Minor change

--- accelerator-core/src/main/java/com/opentok/accelerator/annotation/AnnotationsView.java
+++ accelerator-core/src/main/java/com/opentok/accelerator/annotation/AnnotationsView.java
@@ -751,7 +751,7 @@ public class AnnotationsView extends ViewGroup implements AnnotationsToolbar.Act
                     localWidth = getDisplayWidth();
                 }
                 if (localHeight == 0) {
-                    localHeight =  Float.parseFloat(mVideoHeight);
+                    localHeight =  ((Number) mVideoHeight).floatValue();
                     Log.d(LOG_TAG, "penAnnotations local Height "+localHeight);
                 }
 
